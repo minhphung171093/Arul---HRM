a3ea63adb9c6aef5519b22de9b212f4011a65a49
tienpham <tienpham@tienpham.(none)>
2014-11-11 08:33:10 +0700

    update

--- green_erp_arulmani_hrm/res_company.py
+++ green_erp_arulmani_hrm/res_company.py
@@ -6,4 +6,6 @@ import time
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP, float_compare
 from datetime import datetime
 
+
+#ufhgnxifgifg
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
