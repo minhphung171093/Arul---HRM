9673f0e994f6167c056df8b9c2cce4c043c42b6d
tienpham <tienpham@tienpham.(none)>
2014-11-11 14:41:25 +0700

    update

--- green_erp_arulmani_hrm/res_company.py
+++ green_erp_arulmani_hrm/res_company.py
@@ -7,5 +7,18 @@ from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FO
 from datetime import datetime
 
 
-#ufhgnxifgifg
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+class res_company(osv.osv):
+    _inherit = 'res.company'
+    _columns = {
+                'company_code': fields.char('Street', size=128),
+                'reg_name': fields.char('Name', size=128),
+                'reg_street': fields.char('Street', size=128),
+                'reg_zip': fields.char('Zip', change_default=True, size=24),
+                'reg_city': fields.char('City', size=128),
+                'reg_state_id': fields.many2one("res.country.state", 'State', domain="[('country_id', '=', country_id)]"),
+                'reg_country_id': fields.many2one('res.country', 'Country'),
+                }    
+res_company()   
