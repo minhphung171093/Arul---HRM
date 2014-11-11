b49715b90cd59fe16ebc6f7013f48b13c5705160
phuoc <phuoc@phuoc-X401A.(none)>
2014-11-11 10:36:50 +0700

    Payroll Sub Area DOne !!!

--- green_erp_arulmani_hrm/hr_payroll.py
+++ green_erp_arulmani_hrm/hr_payroll.py
@@ -6,8 +6,7 @@ import time
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP, float_compare
 from datetime import datetime
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-# _logger = logging.getLogger(__name__)
+
 
 class arul_hr_payroll_area(osv.osv):
     _name = 'arul.hr.payroll.area'
@@ -16,14 +15,33 @@ class arul_hr_payroll_area(osv.osv):
          'code': fields.char('Code', size=1024, required = True),
         
     }
-    def _check_company_id(self, cr, uid, ids, context=None):
+    def _check_code_id(self, cr, uid, ids, context=None):
         for payroll in self.browse(cr, uid, ids, context=context):
             payroll_ids = self.search(cr, uid, [('id','!=',payroll.id),('code','=',payroll.code)])
             if payroll_ids:  
                 return False
         return True
     _constraints = [
-        (_check_company_id, 'Identical Data', ['code']),
+        (_check_code_id, 'Identical Data', ['code']),
     ]
     
 arul_hr_payroll_area()
+
+class arul_hr_payroll_sub_area(osv.osv):
+    _name = 'arul.hr.payroll.sub.area'
+    _columns = {
+        'name': fields.char('Name', size=1024, required = True),
+         'code': fields.char('Code', size=1024, required = True),
+        
+    }
+    def _check_code_id(self, cr, uid, ids, context=None):
+        for payroll in self.browse(cr, uid, ids, context=context):
+            payroll_ids = self.search(cr, uid, [('id','!=',payroll.id),('code','=',payroll.code)])
+            if payroll_ids:  
+                return False
+        return True
+    _constraints = [
+        (_check_code_id, 'Identical Data', ['code']),
+    ]
+    
+arul_hr_payroll_sub_area()
