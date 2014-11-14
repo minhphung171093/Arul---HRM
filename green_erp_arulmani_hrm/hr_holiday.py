302d3ba8cc0dd9772fb37b64719de305e63dcc56
phuoc <phuoc@phuoc-X401A.(none)>
2014-11-14 16:33:26 +0700

    update

--- green_erp_arulmani_hrm/hr_holiday.py
+++ green_erp_arulmani_hrm/hr_holiday.py
@@ -15,3 +15,15 @@ class arul_hr_holiday_special(osv.osv):
     }
 arul_hr_holiday_special()
 
+class arul_hr_leave_master(osv.osv):
+    _name = "arul.hr.leave.master"
+    _columns = {
+        'leave_type_id' : fields.many2one('arul.hr.leave.types', 'Leave Type'),
+        'employee_category_id' : fields.many2one('vsis.hr.employee.category', 'Employee Category'),
+        'employee_sub_category_id' : fields.many2one('hr.employee.sub.category', 'Employee Sub Category'),
+        'maximum_limit': fields.integer('Maximum Limit Applicable'),
+        'carryforward_nextyear': fields.boolean('Is Carry Forward for Next Year'),
+        'condition': fields.selection([('yes', 'Yes'),('no', 'No')], 'Eligible per Annum'),
+    }
+arul_hr_leave_master()
+
