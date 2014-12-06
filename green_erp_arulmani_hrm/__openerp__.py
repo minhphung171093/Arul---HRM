b06a6e1ef5c9dd9d189bffc9f515e21daa91f2e9
phung11764 <minhphung171093@gmail.com>
2014-12-06 08:17:20 +0700

    update

--- green_erp_arulmani_hrm/__openerp__.py
+++ green_erp_arulmani_hrm/__openerp__.py
@@ -44,7 +44,7 @@
         'hr_payroll_earning_parameters_demo.xml',
         'hr_demo_payroll_deduction_parameters.xml',
         'report/arul_payslip_report.xml',
-        'data_category.xml',
+#         'data_category.xml',
     ],
     'css' : [
         "static/src/css/base.css",
