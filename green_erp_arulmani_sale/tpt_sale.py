d5b63aa8217d526ce1b7672f8611145f42db22a1
TrongNguyen <nguyentrong6193@gmail.com>
2014-12-18 16:31:57 +0700

    update

--- green_erp_arulmani_sale/tpt_sale.py
+++ green_erp_arulmani_sale/tpt_sale.py
@@ -13,8 +13,8 @@ class sale_order(osv.osv):
     _inherit = "sale.order"
     _columns = {
         'order_type':fields.selection([('domestic','Domestic'),('export','Export')],'Order Type' ,required=True),
-        'blanket_id':fields.many2one('tpt.blanket.order','Blanket Order',required = True),
-        'so_date':fields.date('SO Date'),
+        'blanket_id':fields.many2one('tpt.blanket.order','Blanket Order'),
+#         'so_date':fields.date('SO Date'),
         'po_date':fields.date('PO Date'),
         'document_type':fields.selection([('saleorder','Sale Order'),('return','Return Sales Order'),('scrap','Scrap Sales')],'Document Type' ,required=True),
         'po_number':fields.char('PO Number', size = 1024),
@@ -26,32 +26,75 @@ class sale_order(osv.osv):
         'distribution_chanel':fields.many2one('crm.case.channel','Distribution Chanel',required = True),
         'sale_tax':fields.many2one('account.tax','Sales Tax',required = True),
         'excise_duty':fields.char('Excise Duty',size = 1024),
+        'invoice_address': fields.char('Invoice Address', size = 1024),
+        'street2': fields.char('', size = 1024),
+        'city': fields.char('', size = 1024),
+        'country_id': fields.many2one('res.country', ''),
+        'state_id': fields.many2one('res.country.state', ''),
+        'zip': fields.char('', size = 1024),
+        'sale_consignee_line':fields.one2many('tpt.sale.order.consignee','sale_order_consignee_id','Consignee')
     }
     _defaults = {
-        'so_date': time.strftime('%Y-%m-%d'),
+#         'so_date': time.strftime('%Y-%m-%d'),
     }
-    def onchange_blanket_id(self, cr, uid, ids,blanket_id=False, context=None):
-        vals = {}
-        if blanket_id:
-            emp = self.pool.get('tpt.blanket.order').browse(cr, uid, blanket_id)
-            vals = {'partner_id':emp.customer_id.name,
-                    'partner_invoice_id':emp.invoice_address,
-#                     'payment_term_id':emp.payment_term_id.id,
-                    'po_date':emp.po_date,
-                    'order_type':emp.order_type,
-                    'po_number':emp.po_number,
-                    'currency_id':emp.currency_id.id,
-                    'quotaion_no':emp.quotaion_no,
-#                     'incoterms_id':emp.currency_id.id,
-                    'distribution_chanel':emp.po_number,
-                    'currency_id':emp.currency_id.id,
-                    'quotaion_no':emp.quotaion_no,
-                    }
-        return {'value': vals}    
+#     def onchange_blanket_id(self, cr, uid, ids,blanket_id=False, context=None):
+#         vals = {}
+#         if blanket_id:
+#             emp = self.pool.get('tpt.blanket.order').browse(cr, uid, blanket_id)
+#             vals = {'partner_id':emp.customer_id.name,
+#                     'partner_invoice_id':emp.invoice_address,
+#                     'payment_term':emp.payment_term_id.id,
+#                     'po_date':emp.po_date,
+#                     'order_type':emp.order_type,
+#                     'po_number':emp.po_number,
+#                     'currency_id':emp.currency_id.id,
+#                     'quotaion_no':emp.quotaion_no,
+#                     'incoterms_id':emp.incoterm_id.id,
+#                     'distribution_chanel':emp.po_number,
+#                     'currency_id':emp.currency_id.id,
+#                     'quotaion_no':emp.quotaion_no,
+#                     'excise_duty':emp.excise_duty,
+#                     'sale_tax':emp.sale_tax_id.id,
+#                     'reason':emp.reason,
+#                     }
+#         return {'value': vals}    
 
 sale_order()
 
-
+class tpt_sale_order_consignee(osv.osv):
+    _name = "tpt.sale.order.consignee"
+    
+    def quatity_consignee(self, cr, uid, ids, field_name, args, context=None):
+        quatity = 0
+        res = {}
+        for line in self.browse(cr,uid,ids,context=context):
+            for order_line in line.blanket_consignee_id.blank_order_line:
+                if order_line.product_id.id == line.product_id.id:
+                    quatity = order_line.product_uom_qty
+            res[line.id] = quatity
+        return res
+                
+            
+          
+    _columns = {
+        'sale_order_consignee_id': fields.many2one('sale.order', 'Consignee'),
+        'name': fields.char('Consignee Name', size = 1024, required = True),
+        'location': fields.char('Location', size = 1024),
+        'product_id': fields.many2one('product.product', 'Product'),
+        'product_uom_qty': fields.function(quatity_consignee, store = True, type='float',string='Quatity'),
+        'uom_po_id': fields.many2one('product.uom', 'UOM', readonly = True),
+                }
+    
+    def onchange_product_id(self, cr, uid, ids,product_id=False, context=None):
+        vals = {}
+        if product_id :
+            product = self.pool.get('product.product').browse(cr, uid, product_id)
+            vals = {
+                    'uom_po_id':product.uom_id.id,
+                    }
+        return {'value': vals}
+      
+tpt_sale_order_consignee()
 
 class tpt_blanket_order(osv.osv):
     _name = "tpt.blanket.order"
@@ -183,6 +226,21 @@ class tpt_consignee(osv.osv):
         return {'value': vals}
       
 tpt_consignee()
+
+class tpt_test_report(osv.osv):
+    _name = "tpt.test.report"
+      
+    _columns = {
+        'product_id': fields.many2one('product.product', 'Product', required = True),
+        'grade':fields.char('Grade', size = 1024),
+        'ph':fields.char('pH(5% of Slurry)', size = 1024),
+        'moisture':fields.char('Moisture, % by mass', size = 1024),
+        'iron':fields.char(' Iron, % by mass', size = 1024),
+        'purity':fields.char(' Purity as FSH, % by mass', size = 1024),
+        'ferric':fields.char('Ferric Iron, % by mass', size = 1024),
+        'acid':fields.char('Free Acid, % by mass', size = 1024),
+                }
+tpt_test_report()
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
