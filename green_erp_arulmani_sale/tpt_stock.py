f19be0d143e27ca63cf3d82b353657a843ba9477
phuoc <phuoc@phuoc-X401A.(none)>
2014-12-29 17:53:03 +0700

    update

--- green_erp_arulmani_sale/tpt_stock.py
+++ green_erp_arulmani_sale/tpt_stock.py
@@ -46,26 +46,13 @@ class stock_move(osv.osv):
             }
         return res
     _columns = {
-#         'name': fields.char('Movement ID', size = 1024, required=True),
-#         'warehouse_id': fields.many2one('stock.location', 'Source Warehouse'),
         'product_type': fields.selection([('product', 'Stockable Product'),('consu', 'Consumable'),('service', 'Service')],'Product Type'),  
-#         'do_ref_id': fields.many2one('stock.picking.out','DO Reference'),   
         'application_id': fields.many2one('crm.application','Application'),   
         'prodlot_id': fields.many2one('stock.production.lot', 'System Serial No.', states={'done': [('readonly', True)]}, help="Serial number is used to put a serial number on the production", select=True), 
 #         'sys_batch':fields.many2one('stock.production.lot','System Serial No.'), 
 #         'phy_batch':fields.char('Physical Batch No.', size = 1024)
         'phy_batch':fields.function(get_phy_batch,type='char', size = 1024,string='Physical Serial No.',multi='sum',store=True),
-#         'reason': fields.text("Reason for move"),
-#         'product_id': fields.many2one('product.product', 'Product', required=False, select=True, domain=[('type','<>','service')],states={'done': [('readonly', True)]}),
-#         'product_uom': fields.many2one('product.uom', 'Unit of Measure', required=False,states={'done': [('readonly', True)]}),
+
                 }
-#     _defaults = {
-#         'name': '/',
-#     }
-#     
-#     def create(self, cr, uid, vals, context=None):
-#         if vals.get('name','/')=='/':
-#             vals['name'] = self.pool.get('ir.sequence').get(cr, uid, 'tpt.stock.move.import') or '/'
-#         return super(stock_move, self).create(cr, uid, vals, context=context)
     
 stock_move()
