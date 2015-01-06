8600dc33aa932bdbcea951589f60b615095b0d78
TrongNguyen <nguyentrong6193@gmail.com>
2015-01-06 19:25:46 +0700

    update

--- green_erp_arulmani_crm/__init__.py
+++ green_erp_arulmani_crm/__init__.py
@@ -6,7 +6,7 @@ import green_erp_arulmani_hr
 import crm_specification
 import crm_qc_test
 import crm_sample_invoice
-# import green_erp_arulmani_sale
+import green_erp_arulmani_sale
 import crm_configuration
 import wizard
 import report
