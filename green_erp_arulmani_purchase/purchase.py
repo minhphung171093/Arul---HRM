00f2797be82aebacd492e99f8dbc11e010628f22
TrongNguyen <nguyentrong6193@gmail.com>
2015-01-13 16:25:50 +0700

    update

--- green_erp_arulmani_purchase/purchase.py
+++ green_erp_arulmani_purchase/purchase.py
@@ -7,8 +7,117 @@ from openerp import SUPERUSER_ID
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, DATETIME_FORMATS_MAP, float_compare
 from datetime import datetime
 import datetime
+from datetime import date
 import calendar
 import openerp.addons.decimal_precision as dp
+
+class tpt_purchase_indent(osv.osv):
+    _name = 'tpt.purchase.indent'
+    _columns = {
+        'name': fields.char('Indent No.', size=1024, readonly=True ),
+        'date_indent':fields.date('Indent Date',required = True),
+        'document_type':fields.selection([
+                                ('base','VV Level Based PR'),
+                                ('capital','VV Capital PR'),
+                                ('local','VV Local Purchase PR'),
+                                ('maintenance','VV Maintenance PR'),
+                                ('consumable','VV Consumable PR'),
+                                ('outside','VV Outside Service PR'),
+                                ('spare','VV Spare (Project) PR'),
+                                ('service','VV Service PR'),
+                                ],'Document Type',required = True),
+        'intdent_cate':fields.selection([
+                                ('emergency','Emergency Indent'),
+                                ('normal','Normal Indent')],'Indent Category',required = True),
+        'raised_from_id':fields.many2one('hr.department','Indent Raised From',required = True),
+        'raised_by_id':fields.many2one('hr.employee','Raised By'),
+        'date_expect':fields.date('Expected Date'),
+        'select_normal':fields.selection([('single','Single Quotation'),
+                                          ('special','Special Quotation'),
+                                          ('multiple','Multiple Quotation')],'Select'),
+        'supplier_id':fields.many2one('res.partner','Supplier'),
+        'reason':fields.text('Reason'),
+        'purchase_product_line':fields.one2many('tpt.purchase.product','purchase_indent_id','Product')
+    }
+    _defaults = {
+        'date_indent': fields.datetime.now,
+        'name': '/',
+    }
+    
+    def create(self, cr, uid, vals, context=None):
+        if 'document_type' in vals:
+            sql = '''
+                select code from account_fiscalyear where '%s' between date_start and date_stop
+            '''%(time.strftime('%Y-%m-%d'))
+            cr.execute(sql)
+            fiscalyear = cr.dictfetchone()
+            if not fiscalyear:
+                raise osv.except_osv(_('Warning!'),_('Financial year has not been configured. !'))
+            else:
+                if (vals['document_type']=='base'):
+                    if vals.get('name','/')=='/':
+                        sequence = self.pool.get('ir.sequence').get(cr, uid, 'indent.purchase.based')
+                        vals['name'] =  sequence and sequence+'/'+fiscalyear['code'] or '/'
+                if (vals['document_type']=='capital'):
+                    if vals.get('name','/')=='/':
+                        sequence = self.pool.get('ir.sequence').get(cr, uid, 'indent.purchase.capital')
+                        vals['name'] =  sequence and sequence +'/'+fiscalyear['code']or '/'
+                if (vals['document_type']=='local'):
+                    if vals.get('name','/')=='/':
+                        sequence = self.pool.get('ir.sequence').get(cr, uid, 'indent.purchase.capital')
+                        vals['name'] =  sequence and sequence +'/'+fiscalyear['code']or '/'
+                if (vals['document_type']=='maintenance'):
+                    if vals.get('name','/')=='/':
+                        sequence = self.pool.get('ir.sequence').get(cr, uid, 'indent.purchase.maintenance')
+                        vals['name'] =  sequence and sequence +'/'+fiscalyear['code']or '/'
+                if (vals['document_type']=='consumable'):
+                    if vals.get('name','/')=='/':
+                        sequence = self.pool.get('ir.sequence').get(cr, uid, 'indent.purchase.consumable')
+                        vals['name'] =  sequence and sequence +'/'+fiscalyear['code']or '/'
+                if (vals['document_type']=='outside'):
+                    if vals.get('name','/')=='/':
+                        sequence = self.pool.get('ir.sequence').get(cr, uid, 'indent.purchase.outside')
+                        vals['name'] =  sequence and sequence +'/'+fiscalyear['code']or '/'
+                if (vals['document_type']=='spare'):
+                    if vals.get('name','/')=='/':
+                        sequence = self.pool.get('ir.sequence').get(cr, uid, 'indent.purchase.spare')
+                        vals['name'] =  sequence and sequence +'/'+fiscalyear['code']or '/'
+                if (vals['document_type']=='service'):
+                    if vals.get('name','/')=='/':
+                        sequence = self.pool.get('ir.sequence').get(cr, uid, 'indent.purchase.service')
+                        vals['name'] =  sequence and sequence +'/'+fiscalyear['code']or '/'
+        return super(tpt_purchase_indent, self).create(cr, uid, vals, context=context)    
+    
+    def onchange_date_expect(self, cr, uid, ids,date_indent=False, context=None):
+        vals = {}
+        kq=''
+        if date_indent :
+            sql='''
+            select date(date('%s')+INTERVAL '1 month 1days') as date_indent
+            '''%(date_indent)
+            cr.execute(sql)
+            dates = cr.dictfetchone()['date_indent']
+        return {'value': {'date_expect':dates}}
+
+tpt_purchase_indent()
+class tpt_purchase_product(osv.osv):
+    _name = 'tpt.purchase.product'
+    _columns = {
+        'purchase_indent_id':fields.many2one('tpt.purchase.indent','Purchase Product'),
+        'product_id': fields.many2one('product.product', 'Product',required = True),
+        'product_uom_qty': fields.float('Quantity'),   
+        'uom_po_id': fields.many2one('product.uom', 'UOM', readonly = True),
+        }  
+    
+    def onchange_indent_product_id(self, cr, uid, ids,product_id=False, context=None):
+        vals = {}
+        if product_id :
+            product = self.pool.get('product.product').browse(cr, uid, product_id)
+            vals = {
+                    'uom_po_id':product.uom_id.id,
+                    }
+        return {'value': vals}
+tpt_purchase_product()
 
 
 
