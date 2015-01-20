9d7638d21e6962aef93eed3a3d759443ccf6c075
TrongNguyen <nguyentrong6193@gmail.com>
2015-01-20 16:37:35 +0700

    update

--- green_erp_arulmani_purchase/wizard/tick_purchase_chart.py
+++ green_erp_arulmani_purchase/wizard/tick_purchase_chart.py
@@ -15,14 +15,48 @@ class tick_purchase_chart(osv.osv_memory):
                                                      ('local','VV Local PO'),
                                                      ('return','VV Return PO'),
                                                      ('service','VV Service PO'),
-                                                     ('out','VV Out Service PO')],'PO Document Type'),
+                                                     ('out','VV Out Service PO')],'PO Document Type',required=True),
                 'message': fields.text(string="Message ", readonly=True),    
                 }
             
     def tick_ok(self, cr, uid, ids, context=None):
+        purchase_line = []
+        purchase_order_obj = self.pool.get('purchase.order')
         q_id = context.get('active_id')
+        chart = self.pool.get('tpt.purchase.quotation').browse(cr, uid, q_id)
+        location_ids=self.pool.get('stock.warehouse').search(cr, uid,[('company_id','=',chart.supplier_id.company_id.id)])
+        location = self.pool.get('stock.warehouse').browse(cr, uid, location_ids[0])
+        vals = purchase_order_obj.onchange_partner_id(cr, uid, [q_id],chart.supplier_id.id)['value']
+        tick = self.browse(cr, uid, ids[0])
         
-        {
-         
-        }
+        for line in chart.:
+            purchase_line.append((0,0,{
+                                       
+                                       }))
+        vals.update({
+                     'po_document_type': tick.po_document_type,
+                    'quotation_no':chart.id,
+                    'partner_id':chart.supplier_id.id,
+                    'partner_ref':chart.quotation_ref,
+                    'purchase_tax_id':chart.tax_id.id,
+                    'date_order':chart.date_quotation,
+                    'location_id':location.lot_stock_id.id,
+                    'invoice_method': 'picking',
+                    'order_line': purchase_line,
+                    })
+        new_po_id = purchase_order_obj.create(cr, uid, vals)
+        res = self.pool.get('ir.model.data').get_object_reference(cr, uid, 
+                                        'purchase', 'purchase_order_form')
+        return {
+                    'name': 'Purchase Order',
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'view_id': res[1],
+                    'res_model': 'purchase.order',
+                    'domain': [],
+                    'type': 'ir.actions.act_window',
+                    'target': 'current',
+                    'res_id': new_po_id,
+                }
+        
 tick_purchase_chart()
