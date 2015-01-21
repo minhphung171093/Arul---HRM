3f42d4d5e1fe5c15c02c13ff1161f41b3cd4baca
tien <tien@tien-SVE15127CVW.(none)>
2015-01-21 17:43:25 +0700

    update

--- green_erp_arulmani_purchase/purchase.py
+++ green_erp_arulmani_purchase/purchase.py
@@ -391,10 +391,10 @@ class tpt_gate_in_pass(osv.osv):
             gate_in_pass_line = []
             for line in po.order_line:
                 gate_in_pass_line.append({
-#                             'po_indent_no': line.
-                          'product_id': line.product_id.id,
-                          'product_qty':line.product_qty,
-                          'uom_po_id': line.product_uom.id,
+                            'po_indent_no': line.po_indent_no and line.po_indent_no.id or False,
+                          'product_id': line.product_id and line.product_id.id or False,
+                          'product_qty':line.product_qty or False,
+                          'uom_po_id': line.product_uom and line.product_uom.id or False,
                     })
         res['value'].update({
                     'supplier_id':po.partner_id and po.partner_id.id or False,
