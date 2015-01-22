44bfc2fce6ec9e02521f562e9899c12bffac046d
phuoc <phuoc@phuoc-X401A.(none)>
2015-01-22 08:33:35 +0700

    update

--- green_erp_arulmani_purchase/purchase.py
+++ green_erp_arulmani_purchase/purchase.py
@@ -835,7 +835,7 @@ class purchase_order_line(osv.osv):
     
     
     _columns = {
-        'po_indent_no' : fields.many2one('tpt.purchase.indent', 'PO Indent No', required = True),
+        'po_indent_no' : fields.many2one('tpt.purchase.indent', 'PO Indent No'),
                 }
     _defaults = {
                  'date_planned':time.strftime('%Y-%m-%d'),
@@ -844,7 +844,129 @@ class purchase_order_line(osv.osv):
         if po_indent_no:
             return {'value': {'product_id': False}}    
         
-    
+    def onchange_product_uom(self, cr, uid, ids, pricelist_id, product_id, qty, uom_id,
+            partner_id, date_order=False, fiscal_position_id=False, date_planned=False,
+            name=False, price_unit=False, context=None):
+        """
+        onchange handler of product_uom.
+        """
+        if context is None:
+            context = {}
+        if not uom_id:
+            return {'value': {'price_unit': price_unit or 0.0, 'name': name or '', 'product_uom' : uom_id or False}}
+        context = dict(context, purchase_uom_check=True)
+        return self.onchange_product_id(cr, uid, ids, pricelist_id, product_id, qty, uom_id,
+            partner_id, date_order=date_order, fiscal_position_id=fiscal_position_id, date_planned=date_planned,
+            name=name, price_unit=price_unit, context=context)
+ 
+  
+    def onchange_product_id(self, cr, uid, ids, pricelist_id, product_id, qty, uom_id,
+            partner_id, date_order=False, fiscal_position_id=False, date_planned=False,
+            name=False, price_unit=False, context=None,  po_indent_no = False):
+        """
+        onchange handler of product_id.
+        """
+        if context is None:
+            context = {}
+ 
+        res = {'value': {'price_unit': price_unit or 0.0, 'name': name or '', 'product_uom' : uom_id or False}}
+        if po_indent_no: 
+            vals = {}
+            if product_id:
+                po = self.pool.get('tpt.purchase.indent').browse(cr, uid, po_indent_no)
+                product = self.pool.get('product.product').browse(cr, uid, product_id)
+                for line in po.purchase_product_line:
+                    if product_id == line.product_id.id:
+                        vals = {
+                                'price_unit':product.standard_price,
+                                'uom_po_id':line.uom_po_id and line.uom_po_id.id or False,
+                                'product_uom_qty':line.product_uom_qty or False,
+                                }
+                        return {'value': vals}  
+        if not product_id:
+            return res
+ 
+        product_product = self.pool.get('product.product')
+        product_uom = self.pool.get('product.uom')
+        res_partner = self.pool.get('res.partner')
+        product_supplierinfo = self.pool.get('product.supplierinfo')
+        product_pricelist = self.pool.get('product.pricelist')
+        account_fiscal_position = self.pool.get('account.fiscal.position')
+        account_tax = self.pool.get('account.tax')
+ 
+        # - check for the presence of partner_id and pricelist_id
+        #if not partner_id:
+        #    raise osv.except_osv(_('No Partner!'), _('Select a partner in purchase order to choose a product.'))
+        #if not pricelist_id:
+        #    raise osv.except_osv(_('No Pricelist !'), _('Select a price list in the purchase order form before choosing a product.'))
+ 
+        # - determine name and notes based on product in partner lang.
+        context_partner = context.copy()
+         
+        if partner_id:
+            lang = res_partner.browse(cr, uid, partner_id).lang
+            context_partner.update( {'lang': lang, 'partner_id': partner_id} )
+        product = product_product.browse(cr, uid, product_id, context=context_partner)
+        #call name_get() with partner in the context to eventually match name and description in the seller_ids field
+        dummy, name = product_product.name_get(cr, uid, product_id, context=context_partner)[0]
+        if product.description_purchase:
+            name += '\n' + product.description_purchase
+        res['value'].update({'name': name})
+ 
+        # - set a domain on product_uom
+        res['domain'] = {'product_uom': [('category_id','=',product.uom_id.category_id.id)]}
+ 
+        # - check that uom and product uom belong to the same category
+        product_uom_po_id = product.uom_po_id.id
+        if not uom_id:
+            uom_id = product_uom_po_id
+ 
+        if product.uom_id.category_id.id != product_uom.browse(cr, uid, uom_id, context=context).category_id.id:
+            if context.get('purchase_uom_check') and self._check_product_uom_group(cr, uid, context=context):
+                res['warning'] = {'title': _('Warning!'), 'message': _('Selected Unit of Measure does not belong to the same category as the product Unit of Measure.')}
+            uom_id = product_uom_po_id
+ 
+        res['value'].update({'product_uom': uom_id})
+ 
+        # - determine product_qty and date_planned based on seller info
+        if not date_order:
+            date_order = fields.date.context_today(self,cr,uid,context=context)
+ 
+ 
+        supplierinfo = False
+        for supplier in product.seller_ids:
+            if partner_id and (supplier.name.id == partner_id):
+                supplierinfo = supplier
+                if supplierinfo.product_uom.id != uom_id:
+                    res['warning'] = {'title': _('Warning!'), 'message': _('The selected supplier only sells this product by %s') % supplierinfo.product_uom.name }
+                min_qty = product_uom._compute_qty(cr, uid, supplierinfo.product_uom.id, supplierinfo.min_qty, to_uom_id=uom_id)
+                if (qty or 0.0) < min_qty: # If the supplier quantity is greater than entered from user, set minimal.
+                    if qty:
+                        res['warning'] = {'title': _('Warning!'), 'message': _('The selected supplier has a minimal quantity set to %s %s, you should not purchase less.') % (supplierinfo.min_qty, supplierinfo.product_uom.name)}
+                    qty = min_qty
+        dt = self._get_date_planned(cr, uid, supplierinfo, date_order, context=context).strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        qty = qty or 1.0
+        res['value'].update({'date_planned': date_planned or dt})
+        if qty:
+            res['value'].update({'product_qty': qty})
+ 
+        # - determine price_unit and taxes_id
+        if pricelist_id:
+            price = product_pricelist.price_get(cr, uid, [pricelist_id],
+                    product.id, qty or 1.0, partner_id or False, {'uom': uom_id, 'date': date_order})[pricelist_id]
+        else:
+            price = product.standard_price
+ 
+        taxes = account_tax.browse(cr, uid, map(lambda x: x.id, product.supplier_taxes_id))
+        fpos = fiscal_position_id and account_fiscal_position.browse(cr, uid, fiscal_position_id, context=context) or False
+        taxes_ids = account_fiscal_position.map_tax(cr, uid, fpos, taxes)
+        res['value'].update({'price_unit': price, 'taxes_id': taxes_ids})
+ 
+        return res
+ 
+    product_id_change = onchange_product_id
+    product_uom_change = onchange_product_uom    
+      
 #     def onchange_product_id(self, cr, uid, ids, product_id=False, po_indent_no=False, context=None):
 #         vals = {}
 #         if po_indent_no and product_id: 
