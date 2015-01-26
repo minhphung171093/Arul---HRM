2e51b6c21d051044db48822c74fca217d1e702bb
tien <tien@tien-SVE15127CVW.(none)>
2015-01-26 15:30:33 +0700

    update

--- green_erp_arulmani_sale/report/packing_list_report.py
+++ green_erp_arulmani_sale/report/packing_list_report.py
@@ -29,6 +29,7 @@ class Parser(report_sxw.rml_parse):
             'get_qty_mt': self.get_qty_mt,
             'get_qty_kg':self.get_qty_kg,
             'get_buyer': self.get_buyer,
+            'get_consignee': self.get_consignee,
         })
     
     def get_invoice_info(self, do_id):
@@ -70,40 +71,76 @@ class Parser(report_sxw.rml_parse):
             bags_qty = qty/25 + 'NOS OF HDPE LINED BAGS (25KGS BAGS)'
         if uom.lower()=='bags':
             bags_qty = qty
-        if uom.lower()=='mt' and type == 'domestic':
+        if uom.lower()=='tonne' and type == 'domestic':
             bags_qty = qty*1000/50 + 'NOS OF HDPE LINED BAGS (50KGS BAGS)'
-        if uom.lower()=='mt' and type == 'export':
+        if uom.lower()=='tonne' and type == 'export':
             bags_qty = qty*1000/25 + 'NOS OF HDPE LINED BAGS (25KGS BAGS)'
         return bags_qty
     
     def get_qty_mt(self, qty, uom, type):
         mt_qty = 0.0
-        if uom.lower()=='kg':
-            mt_qty = qty/1000
-        if uom.lower()=='bags' and type == 'domestic':
-            mt_qty = qty*50/1000
-        if uom.lower()=='bags' and type == 'export':
-            mt_qty = qty*25/1000
-        if uom.lower()=='mt':
-            mt_qty = qty
+        if uom:
+            unit = uom.replace(' ','')
+            if unit.lower()=='kg':
+                mt_qty = qty/1000
+            if unit.lower()=='bags' and type == 'domestic':
+                mt_qty = qty*50/1000
+            if unit.lower()=='bags' and type == 'export':
+                mt_qty = qty*25/1000
+            if unit.lower()=='tonne':
+                mt_qty = qty
         return mt_qty
     
     def get_qty_kg(self, qty, uom, type):
         mt_qty = 0.0
-        if uom.lower()=='kg':
-            mt_qty = qty
-        if uom.lower()=='bags' and type == 'domestic':
-            mt_qty = qty*50
-        if uom.lower()=='bags' and type == 'export':
-            mt_qty = qty*25
-        if uom.lower()=='mt':
-            mt_qty = qty*1000
+        if uom:
+            unit = uom.replace(' ','')
+            if unit.lower()=='kg':
+                mt_qty = qty
+            if unit.lower()=='bags' and type == 'domestic':
+                mt_qty = qty*50
+            if unit.lower()=='bags' and type == 'export':
+                mt_qty = qty*25
+            if unit.lower()=='tonne':
+                mt_qty = qty*1000
         return mt_qty
+    
+    def get_consignee(self, obj):
+        cons = ''
+        if obj.partner_id:
+            if obj.partner_id.name:
+                cons += obj.partner_id.name  + ' & '
+            if obj.partner_id.street:
+                cons += obj.partner_id.street + ', '
+            if obj.partner_id.street2:
+                cons += obj.partner_id.street2 + ', '
+            if obj.partner_id.city:
+                cons += obj.partner_id.city + ', '
+            if obj.partner_id.state_id:
+                cons += obj.partner_id.state_id.name + ', '
+            if obj.partner_id.country_id:
+                cons += obj.partner_id.country_id.name  + ', '
+            if obj.partner_id.zip:
+                cons += obj.partner_id.zip
+        return cons
     
     def get_buyer(self, obj):
         buyer = ''
         if obj.partner_id and obj.cons_loca and obj.partner_id.id != obj.cons_loca.id:
-            buyer = (obj.cons_loca and obj.cons_loca.street or '') + ', ' + (obj.cons_loca and obj.cons_loca.street2 or '') + ', ' + (obj.cons_loca and obj.cons_loca.city or '') + ', ' + (obj.cons_loca and (obj.cons_loca.state_id and obj.cons_loca.state_id.name or '') or '') + ', ' + (obj.cons_loca and (obj.cons_loca.country_id and obj.cons_loca.country_id.name or '') or '') + ', ' + (obj.cons_loca and obj.cons_loca.zip or '')
+            if obj.cons_loca:
+                if obj.cons_loca.street:
+                    buyer += obj.cons_loca.street + ', '
+                if obj.cons_loca.street2:
+                    buyer += obj.cons_loca.street2 + ', '
+                if obj.cons_loca.city:
+                    buyer += obj.cons_loca.city + ', '
+                if obj.cons_loca.state_id:
+                    buyer += obj.cons_loca.state_id.name + ', '
+                if obj.cons_loca.country_id:
+                    buyer += obj.cons_loca.country_id.name  + ', '
+                if obj.cons_loca.zip:
+                    buyer += obj.cons_loca.zip
+#             buyer = (obj.cons_loca and obj.cons_loca.street or '') + ', ' + (obj.cons_loca and obj.cons_loca.street2 or '') + ', ' + (obj.cons_loca and obj.cons_loca.city or '') + ', ' + (obj.cons_loca and (obj.cons_loca.state_id and obj.cons_loca.state_id.name or '') or '') + ', ' + (obj.cons_loca and (obj.cons_loca.country_id and obj.cons_loca.country_id.name or '') or '') + ', ' + (obj.cons_loca and obj.cons_loca.zip or '')
         return buyer
     
 
