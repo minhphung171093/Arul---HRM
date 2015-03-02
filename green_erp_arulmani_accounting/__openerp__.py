ad25dd80995f9e5ea3875b05ede6c7947ec82d42
Phung Pham <minhphung171093@gmail.com>
2015-03-02 09:05:34 +0700

    update

---
+++ green_erp_arulmani_accounting/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'VVTi_TPT_ACCOUNTING',
+    'version': '1.0',
+    'category': 'GreenERP',
+    'sequence': 14,
+    'author': 'Tenth Planet',
+    'website' : 'http://www. tenthplanet.in',
+    'depends': [],
+    'data': [
+        'security/green_erp_arulmani_accounting_security.xml',
+        'security/ir.model.access.csv',
+        'tpt_accounting_view.xml',
+    ],
+    'css' : [
+    ],
+    'js' : [
+    ],
+    'qweb': [
+     ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
