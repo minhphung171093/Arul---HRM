c7ace8e709d77103f4ac6dd05e9663423d194dad
tien <tienpham317@gmail.com>
2015-03-04 11:42:50 +0700

    update

--- green_erp_arulmani_purchase/purchase.py
+++ green_erp_arulmani_purchase/purchase.py
@@ -468,8 +468,9 @@ class product_product(osv.osv):
    
     def _check_product(self, cr, uid, ids, context=None):
         for product in self.browse(cr, uid, ids, context=context):
-            product_ids = self.search(cr, uid, [('id','!=',product.id),('name','=',product.name),('default_code', '=',product.default_code)])
-            if product_ids:
+            product_name_ids = self.search(cr, uid, [('id','!=',product.id),('name','=',product.name)])
+            product_code_ids = self.search(cr, uid, [('id','!=',product.id),('default_code', '=',product.default_code)])
+            if product_name_ids or product_code_ids:
                 raise osv.except_osv(_('Warning!'),_('  Product Code and Name should be Unique. !'))
                 return False
             return True
