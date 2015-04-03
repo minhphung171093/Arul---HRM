52e09ed3c40276b52fe96198ffdd1dd7f60eb678
BalamuruganPurushothaman <dev127@TPT043.(none)>
2015-04-03 13:16:11 +0530

    TPT-Custome Export Inv

--- green_erp_arulmani_sale/tpt_stock.py
+++ green_erp_arulmani_sale/tpt_stock.py
@@ -981,6 +981,7 @@ class account_invoice(osv.osv):
         'port_of_loading_id': fields.char('Port Of Loading', size = 1024, readonly=True, states={'draft':[('readonly',False)]}),
         'port_of_discharge_id': fields.char('Port Of Discharge', size = 1024, readonly=True, states={'draft':[('readonly',False)]}),
         'disc_goods': fields.text('Disciption Of Goods', size = 1024, readonly=True, states={'draft':[('readonly',False)]}),
+        'final_desti': fields.char('Final Destination', size = 1024, readonly=True, states={'draft':[('readonly',False)]}),
         #TPT
         'street3':fields.char('Street3',size=128),
         'fsh_grade':fields.char('FSH Grade',size=128),
