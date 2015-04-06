d26ac4d128c25877e2995942c796a04a2243098b
Phung Pham <minhphung171093@gmail.com>
2015-04-06 15:12:13 +0700

    update

--- green_erp_arulmani_production/tpt_production.py
+++ green_erp_arulmani_production/tpt_production.py
@@ -118,13 +118,14 @@ class tpt_tio2_batch_split(osv.osv):
                 temp = 0
                 try:
                     cr.execute("SELECT setval('ir_sequence_%03d',nextval('ir_sequence_%03d')-1)+1" % (seq['id'],seq['id']))
+                    seq['number_next'] = cr.fetchone()
                 except Exception, e:
                     cr.rollback()
                     temp = 1
                     pass
-                if temp==1:
-                    cr.execute("SELECT setval('ir_sequence_%03d',nextval('ir_sequence_%03d')-1)+1" % (seq['id'],seq['id']))
-                seq['number_next'] = cr.fetchone()
+#                 if temp==1:
+#                     cr.execute("SELECT setval('ir_sequence_%03d',nextval('ir_sequence_%03d')-1)+1" % (seq['id'],seq['id']))
+                seq['number_next'] = 1
             else:
                 cr.execute("SELECT number_next FROM ir_sequence WHERE id=%s FOR UPDATE NOWAIT", (seq['id'],))
             d = self.pool.get('ir.sequence')._interpolation_dict()
