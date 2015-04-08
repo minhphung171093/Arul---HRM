85289223a293329c593b52a310eca474d2385e54
BalamuruganPurushothaman <balamurugan.purushothaman@tenthplanettechnologies.com>
2015-04-08 16:51:30 +0530

    TPT-Inv Print

--- green_erp_arulmani_sale/report/export_fob_invoice_report.py
+++ green_erp_arulmani_sale/report/export_fob_invoice_report.py
@@ -73,7 +73,7 @@ class Parser(report_sxw.rml_parse):
         if (partner.state_id.name).replace(" ", ""):
             return partner.state_id.name+", "+partner.country_id.name
         else:
-            return (partner.country_id.name).upper()
+            return partner.country_id.name
     def get_s3(self,partner):
         if partner.street3:
             return partner.street3+", "+partner.city
