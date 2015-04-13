8180df6e46b4e3a26b64898403506ccd89fd8a76
TrongNguyen <nguyentrong6193@gmail.com>
2015-04-13 17:12:39 +0700

    update

--- green_erp_arulmani_accounting/report/__init__.py
+++ green_erp_arulmani_accounting/report/__init__.py
@@ -4,3 +4,4 @@ import daily_sale_report
 import report_bank_book
 import report_cash_book
 import w_report
+import customer_ledger_statement_report
--- green_erp_arulmani_accounting/wizard/__init__.py
+++ green_erp_arulmani_accounting/wizard/__init__.py
@@ -4,4 +4,5 @@ import daily_sale_form
 import cash_book_report
 import bank_book_report
 import w_form
+import customer_ledger_statement
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
