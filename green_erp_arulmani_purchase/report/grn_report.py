f9f263f960c649be5d55ffe2d09e88be71b8b446
tien <tienpham317@gmail.com>
2015-04-15 17:21:25 +0700

    update

--- green_erp_arulmani_purchase/report/grn_report.py
+++ green_erp_arulmani_purchase/report/grn_report.py
@@ -23,49 +23,32 @@ class Parser(report_sxw.rml_parse):
         super(Parser, self).__init__(cr, uid, name, context=context)
         pool = pooler.get_pool(self.cr.dbname)
         self.localcontext.update({
-#             'get_date': self.get_date,
-#             'get_qty_bags': self.get_qty_bags,
-#             'get_qty_mt': self.get_qty_mt,
+            'convert_date': self.convert_date,
+            'convert_date_time': self.convert_date_time,
             
         })
     
-#     def get_date(self, date=False):
-#         if not date:
-#             date = time.strftime(DATETIME_FORMAT)
-#         date = datetime.strptime(date, DATETIME_FORMAT)
-#         return date.strftime('%d/%m/%Y')
-# 
-# 
-#     
-#     def get_qty_bags(self, qty, uom, type):
-#         bags_qty = 0.0
-#         if uom:
-#             unit = uom.replace(' ','')
-#             if unit.lower()=='kg' and type == 'domestic':
-#                 bags_qty = str(qty/50) + ' NOS OF HDPE LINED BAGS (50KGS BAGS)'
-#             if unit.lower()=='kg' and type == 'export':
-#                 bags_qty = str(qty/25) + ' NOS OF HDPE LINED BAGS (25KGS BAGS)'
-#             if unit.lower()=='bags':
-#                 bags_qty = qty
-#             if unit.lower()=='tonne' and type == 'domestic':
-#                 bags_qty = str(qty*1000/50) + ' NOS OF HDPE LINED BAGS (50KGS BAGS)'
-#             if unit.lower()=='tonne' and type == 'export':
-#                 bags_qty = str(qty*1000/25) + ' NOS OF HDPE LINED BAGS (25KGS BAGS)'
-#         return bags_qty
-#     
-#     def get_qty_mt(self, qty, uom, type):
-#         mt_qty = 0.0
-#         if uom:
-#             unit = uom.replace(' ','')
-#             if unit.lower() in ['kg','kgs']:
-#                 mt_qty = qty/1000
-#             if unit.lower()=='bags' and type == 'domestic':
-#                 mt_qty = qty*50/1000
-#             if unit.lower()=='bags' and type == 'export':
-#                 mt_qty = qty*25/1000
-#             if unit.lower() in ['tonne','tonnes','mt','metricton','metrictons']:
-#                 mt_qty = qty
-#         return round(mt_qty,3)
+    def convert_date(self, date):
+        date = datetime.strptime(date, DATE_FORMAT)
+        return date.strftime('%d/%m/%Y')
+    
+    def convert_date_time(self, date):
+        date = datetime.strptime(date, DATETIME_FORMAT)
+        return date.strftime('%d/%m/%Y')
+    
+#     def get_date(self,date):
+#         res = {}
+#         date = time.strftime('%d/%m/%Y'),
+#         date = datetime.strptime(date[0], DATE_FORMAT)
+#         day = date.day
+#         month = date.month
+#         year = date.year
+#         res = {
+#                'day': day,
+#                'month': month,
+#                'year': year,
+#                }
+#         return res
     
 
     
