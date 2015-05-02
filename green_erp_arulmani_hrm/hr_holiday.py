3b02193851a079539f688e915cde346c9059c593
BalamuruganPurushothaman <balamurugan.purushothaman@tenthplanettechnologies.com>
2015-05-02 14:57:12 +0530

    TPT-hr

--- green_erp_arulmani_hrm/hr_holiday.py
+++ green_erp_arulmani_hrm/hr_holiday.py
@@ -2123,7 +2123,7 @@ class arul_hr_permission_onduty(osv.osv):
 #         'detail_id':fields.many2one('arul.hr.employee.attendence.details','Detail'),
         #TPT-Permission On Duty
         'shift_type':fields.char('Shift Type', size = 1024),
-        'total_shift_worked': fields.function(_shift_total, store=True, string='No.Of Shift Worked', multi='shift_sums', help="The total amount."),
+        'total_shift_worked': fields.function(_shift_total, string='No.Of Shift Worked', multi='shift_sums', help="The total amount."),
         'state':fields.selection([('draft', 'Draft'),('cancel', 'Reject'),('done', 'Approve')],'Status', readonly=True),
               }
     _defaults = {
