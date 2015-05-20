bea6128362c846677d4fea331443f15da4880072
phuoc <your_email@example.com>
2015-05-20 16:31:27 +0700

    cash book report

--- green_erp_arulmani_accounting/wizard/cash_book_report.py
+++ green_erp_arulmani_accounting/wizard/cash_book_report.py
@@ -11,6 +11,7 @@ class tpt_cash_book(osv.osv_memory):
     _name = "tpt.cash.book"
     _columns = {
         'name': fields.char('SI.No', readonly=True),
+        'cash_acc_no': fields.char('SI.No', readonly=True),
         'cb_line': fields.one2many('tpt.cash.book.line', 'cb_id', 'Cash Book Line'),
         'date_from': fields.date('Date From'),
         'date_to': fields.date('Date To'),
@@ -41,9 +42,15 @@ class tpt_cash_book_line(osv.osv_memory):
     _name = "tpt.cash.book.line"
     _columns = {
         'cb_id': fields.many2one('tpt.cash.book', 'Cash Book', ondelete='cascade'),
-        'voucher_id': fields.many2one('account.voucher', 'Voucher No.'),
-        'opening_balance': fields.char(''),
+        'voucher_id': fields.char('Voucher No.', size = 1024),
+        'opening_balance': fields.char('', size = 1024),
         'debit': fields.float('Debit (Rs.)'),
+        'crebit': fields.float('Credit (Rs.)'),
+        'balance': fields.float('Balance (Rs.)'),
+        'date': fields.date('Date'),
+        'desc': fields.char('Description', size = 1024),
+        'gl_code': fields.char('GL Code', size = 1024),
+        'ref': fields.char('Reference', size = 1024),
     }
 
 tpt_cash_book_line()
@@ -67,6 +74,135 @@ class cash_book_report(osv.osv_memory):
     ]
     
     def print_report(self, cr, uid, ids, context=None):
+        
+        def get_line_balance(seq,o):  
+            move = get_move_ids(o)
+            opening_balance = get_opening_balance(o)
+            balance = 0.0
+            for i in range(0, seq+1):
+                balance += (move[i]['credit'] - move[i]['debit'])
+            return opening_balance + balance
+        
+        def get_account_master_name():  
+            sql = '''
+                SELECT name FROM account_account
+                    WHERE name LIKE '%CASH IN HAND%'
+            '''
+            cr.execute(sql)
+            name = cr.dictfetchone()
+            return name and name['name'] or ''
+        
+        def get_account_master_code():  
+            sql = '''
+                SELECT code FROM account_account
+                    WHERE code LIKE '%0000110001%'
+            '''
+            cr.execute(sql)
+            code = cr.dictfetchone()
+            return code and code['code'] or ''
+        
+        def get_opening_balance(o):  
+            date_from = o.date_from
+            date_to = o.date_to
+            type = o.type_trans
+            balance = 0.0  
+            credit = 0.0
+            debit = 0.0
+            sql = '''
+                select sum(aml.credit) as credit, aml.date from account_move_line aml 
+                where aml.credit is not null and aml.credit != 0 and aml.date < '%s' 
+                and move_id in (select move_id from account_voucher where type_trans = 'payment') 
+                group by aml.date
+            '''%(date_from)
+            cr.execute(sql)
+            for move in cr.dictfetchall():
+                credit += move['credit']
+                
+            sql = '''
+                select sum(aml.debit) as debit, aml.date from account_move_line aml 
+                where aml.debit is not null and aml.debit != 0 and aml.date < '%s' 
+                and move_id in (select move_id from account_voucher where type_trans = 'receipt') 
+                group by aml.date
+            '''%(date_from)
+            cr.execute(sql)
+            for move in cr.dictfetchall():
+                debit += move['debit']    
+            balance = debit - credit
+            return balance
+        
+        def get_move_ids(o):
+            account_voucher_obj = self.pool.get('account.voucher')
+            move_lines = []
+            date_arr = []
+            date_from = o.date_from
+            date_to = o.date_to
+            type = o.type_trans
+            if type == 'payment':
+                account_ids = account_voucher_obj.search(cr,uid,[('date', '>=', date_from),('date', '<=', date_to), ('type_cash_bank', '=', 'cash'), ('type_trans', '=', 'payment')])
+                if account_ids:
+                    cr.execute('''
+                        select aa.name as acc_name, aml.account_id, sum(aml.debit) as debit, sum(aml.credit) as credit,av.name as voucher_name,av.date as voucher_date, aml.ref as ref from account_account aa, account_move_line aml,account_voucher av where av.move_id = aml.move_id and
+                        aml.move_id in (select move_id from account_voucher where id in %s and type_trans = 'payment') and debit is not null and debit !=0 and aa.id = aml.account_id group by av.name,aa.name, aml.account_id,av.date, aml.ref order by av.date
+                    ''',(tuple(account_ids),))
+                    return cr.dictfetchall()
+                else:
+                    return []
+            elif type == 'receipt':
+                account_ids = account_voucher_obj.search(cr,uid,[('date', '>=', date_from),('date', '<=', date_to), ('type_cash_bank', '=', 'cash'), ('type_trans', '=', 'receipt')])
+                if account_ids:
+                    cr.execute('''
+                        select aa.name as acc_name, aml.account_id, sum(aml.debit) as debit, sum(aml.credit) as credit,av.name as voucher_name,av.date as voucher_date, aml.ref as ref from account_account aa, account_move_line aml,account_voucher av where av.move_id = aml.move_id and
+                        aml.move_id in (select move_id from account_voucher where id in %s and type_trans = 'receipt') and credit is not null and credit !=0 and aa.id = aml.account_id group by av.name,aa.name, aml.account_id,av.date, aml.ref order by av.date
+                    
+                    ''',(tuple(account_ids),))
+                    return cr.dictfetchall()
+                else:
+                    return []
+            else:
+                account_ids = account_voucher_obj.search(cr,uid,[('date', '>=', date_from),('date', '<=', date_to), ('type_cash_bank', '=', 'cash')])
+                if account_ids:
+                    cr.execute('''
+                        select foo.acc_name, foo.account_id, sum(foo.debit) as debit, sum(foo.credit) as credit,foo.voucher_name,foo.voucher_date, foo.ref from
+                        (select aa.name as acc_name, aml.account_id, aml.debit as debit, aml.credit as credit,av.name as voucher_name,av.date as voucher_date , aml.ref as ref from account_account aa, account_move_line aml,account_voucher av where av.move_id = aml.move_id and
+                        aml.move_id in (select move_id from account_voucher where id in %s and type_trans = 'payment') and aml.debit is not null and aml.debit !=0 and aa.id = aml.account_id
+                        union all
+                        select aa.name as acc_name, aml.account_id, aml.debit as debit, aml.credit as credit,av.name as voucher_name,av.date as voucher_date, aml.ref as ref from account_account aa, account_move_line aml,account_voucher av where av.move_id = aml.move_id and
+                        aml.move_id in (select move_id from account_voucher where id in %s and type_trans = 'receipt') and aml.credit is not null and aml.credit !=0 and aa.id = aml.account_id
+                        )foo
+                        group by foo.acc_name, foo.account_id, foo.voucher_name,foo.voucher_date, foo.ref order by foo.voucher_date
+                    ''',(tuple(account_ids),tuple(account_ids),))
+                    return cr.dictfetchall()
+                else:
+                    return []
+        def get_code_account(code_id):
+            code = ''
+            if code_id:
+                account = self.pool.get('account.account').browse(cr,uid,code_id)
+                code = account.code
+            return code 
+            
+        def get_total_debit(get_move_ids, get_opening_balance):
+            debit = 0.0
+            for move in get_move_ids:
+                debit += move['credit']    
+            return debit+get_opening_balance
+        
+        def get_total_credit(get_move_ids):
+            credit = 0.0
+            for move in get_move_ids:
+                credit += move['debit']    
+            return credit
+        
+        def get_total_balance(get_move_ids, get_opening_balance):
+            debit = 0.0
+            credit = 0.0
+            balance = 0.0
+            for move in get_move_ids:
+                debit += move['credit']
+                credit += move['debit']      
+            balance = (debit+get_opening_balance) - credit
+            return balance
+        
 #         if context is None:
 #             context = {}
 #         datas = {'ids': context.get('active_ids', [])}
@@ -76,24 +212,42 @@ class cash_book_report(osv.osv_memory):
 #         return {'type': 'ir.actions.report.xml', 'report_name': 'report_cash_book', 'datas': datas}
         cr.execute('delete from tpt_cash_book')
         cb_obj = self.pool.get('tpt.cash.book')
-        line = self.browse(cr, uid, ids[0])
+        cb = self.browse(cr, uid, ids[0])
         cb_line = []
         cb_line.append((0,0,{
             'voucher_id': False,
             'opening_balance': 'Opening Balance:',
-            'debit': 1234,
+            'debit': get_opening_balance(cb),
+
         }))
+        for seq, line in enumerate(get_move_ids(cb)):
+            cb_line.append((0,0,{
+                'voucher_id': line['voucher_name'],
+                'opening_balance': False,
+                'debit': line['credit'] and line['credit'] or 0,
+                'crebit': line['debit'] and line['debit'] or 0,
+                'balance': get_line_balance(seq,cb),
+                'date': line['voucher_date'],
+                'desc': line['acc_name'],
+                'gl_code': get_code_account(line['account_id']),
+                'ref': line['ref'],
+            }))
         cb_line.append((0,0,{
             'voucher_id': False,
-            'opening_balance': False,
-            'debit': 123,
+            'opening_balance': 'Days Total',
+            'debit': get_total_debit(get_move_ids(cb), get_opening_balance(cb)),
+            'crebit': get_total_credit(get_move_ids(cb)),
+            'balance': get_total_balance(get_move_ids(cb), get_opening_balance(cb)),
+            'date':False,
+            'desc':False,
         }))
         vals = {
             'name': 'Cash Book for the Period: ',
             'cb_line': cb_line,
-            'date_from': line.date_from,
-            'date_to': line.date_to,
-            'type_trans': line.type_trans,
+            'date_from': cb.date_from,
+            'date_to': cb.date_to,
+            'type_trans': cb.type_trans,
+            'cash_acc_no': 'Cash Account No : '+get_account_master_code()+' - '+ get_account_master_name()
         }
         cb_id = cb_obj.create(cr, uid, vals)
         res = self.pool.get('ir.model.data').get_object_reference(cr, uid, 
