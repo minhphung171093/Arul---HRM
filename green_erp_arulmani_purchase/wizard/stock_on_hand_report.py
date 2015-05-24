f74b9368aae4db6f829d3294dc66cf8405131406
tien <tienpham317@gmail.com>
2015-05-24 23:42:50 +0700

    update

--- green_erp_arulmani_purchase/wizard/stock_on_hand_report.py
+++ green_erp_arulmani_purchase/wizard/stock_on_hand_report.py
@@ -8,6 +8,52 @@ import openerp.tools
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT, float_compare
 
 
+class tpt_stock_on_hand(osv.osv_memory):
+    _name = "tpt.stock.on.hand"
+    _columns = {
+        'name': fields.char('', readonly=True),
+        'categ_id': fields.many2one('product.category', 'Material Category', ondelete='cascade'),
+        'product_id': fields.many2one('product.product', 'Material Code', ondelete='cascade'),
+        'location_id': fields.many2one('stock.location', 'Warehouse Location', ondelete='cascade'),
+        'stock_line': fields.one2many('tpt.stock.on.hand.line', 'stock_id', 'Stock Line'),
+        'as_date': fields.date('As on Date'),
+    }
+    
+#     def print_xls(self, cr, uid, ids, context=None):
+#         if context is None:
+#             context = {}
+#         datas = {'ids': context.get('active_ids', [])}
+#         datas['model'] = 'tpt.bank.book'
+#         datas['form'] = self.read(cr, uid, ids)[0]
+#         datas['form'].update({'active_id':context.get('active_ids',False)})
+#         return {'type': 'ir.actions.report.xml', 'report_name': 'report_bank_book_xls', 'datas': datas}
+#     
+    def print_pdf(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        datas = {'ids': context.get('active_ids', [])}
+        datas['model'] = 'tpt.stock.on.hand'
+        datas['form'] = self.read(cr, uid, ids)[0]
+        datas['form'].update({'active_id':context.get('active_ids',False)})
+        return {'type': 'ir.actions.report.xml', 'report_name': 'report_stock_on_hand', 'datas': datas}
+    
+tpt_stock_on_hand()
+
+class tpt_stock_on_hand_line(osv.osv_memory):
+    _name = "tpt.stock.on.hand.line"
+    _columns = {
+        'stock_id': fields.many2one('tpt.stock.on.hand', 'Line'),
+        'code': fields.char('Code', size = 1024),
+        'description': fields.char('Mat.Description', size = 1024),
+        'uom': fields.char('UOM'),
+        'bin_loc': fields.char('Bin Loc.'),
+        'onhand_qty': fields.float('On hand Qty'),
+        'ins_qty': fields.float('Ins.Qty'),
+        'bl_qty': fields.float('Bl.Qty'),
+    }
+
+tpt_stock_on_hand_line()
+
 class stock_on_hand_report(osv.osv_memory):
     _name = "stock.on.hand.report"
     _columns = {    
@@ -38,14 +84,184 @@ class stock_on_hand_report(osv.osv_memory):
     
     
     def print_report(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        datas = {'ids': context.get('active_ids', [])}
-        datas['model'] = 'stock.on.hand.report'
-        datas['form'] = self.read(cr, uid, ids)[0]
-        datas['form'].update({'active_id':context.get('active_ids',False)})
-        return {'type': 'ir.actions.report.xml', 'report_name': 'report_stock_on_hand', 'datas': datas}
+#         if context is None:
+#             context = {}
+#         datas = {'ids': context.get('active_ids', [])}
+#         datas['model'] = 'stock.on.hand.report'
+#         datas['form'] = self.read(cr, uid, ids)[0]
+#         datas['form'].update({'active_id':context.get('active_ids',False)})
+#         return {'type': 'ir.actions.report.xml', 'report_name': 'report_stock_on_hand', 'datas': datas}
+#         def convert_date(self,date):
+#             if date:
+#                 date = datetime.strptime(date, DATE_FORMAT)
+#                 return date.strftime('%d/%m/%Y')
+    
+#         def get_date(o):
+#             date = time.strftime('%Y-%m-%d'),
+#             date = datetime.strptime(date[0], DATE_FORMAT)
+#             return date.strftime('%d/%m/%Y')
         
+        def get_warehouse(o):
+            loc = o.location_id
+            loc_obj = self.pool.get('stock.location').browse(cr,uid,loc[0])
+            return loc_obj   
+        
+        def get_category(o):
+            cat = o.categ_id
+            if cat:
+                cat_obj = self.pool.get('product.category').browse(cr,uid,cat[0])
+                return cat_obj
+            return False
+        
+        def get_product(o):
+            pro = o.product_id
+            if pro:
+                pro_obj = self.pool.get('product.product').browse(cr,uid,pro[0])
+                return pro_obj
+            return False
+    
+        def get_categ(o):
+            loc = o.location_id
+            categ = o.categ_id
+            product = o.product_id
+            pro_obj = self.pool.get('product.product')
+            categ_ids = []
+    
+            if categ and product:
+                sql='''
+                            select product_product.id 
+                            from product_product,product_template 
+                            where product_template.categ_id in(select product_category.id from product_category where product_category.id = %s) 
+                            and product_product.product_tmpl_id = product_template.id and product_product.id = %s ;
+                '''%(categ[0],product[0])
+                cr.execute(sql)
+                categ_ids += [r[0] for r in cr.fetchall()]
+                return self.pool.get('product.product').browse(cr,uid,categ_ids)
+            if categ:
+                sql='''
+                            select product_product.id 
+                            from product_product,product_template 
+                            where product_template.categ_id in(select product_category.id from product_category where product_category.id = %s) 
+                            and product_product.product_tmpl_id = product_template.id;
+                '''%(categ[0])
+                cr.execute(sql)
+                categ_ids += [r[0] for r in cr.fetchall()]
+                return pro_obj.browse(cr,uid,categ_ids)
+            if product and not categ:
+                sql='''
+                            select product_product.id 
+                            from product_product,product_template 
+                            where product_template.categ_id in(select product_category.id from product_category) 
+                            and product_product.product_tmpl_id = product_template.id and product_product.id = %s ;
+                '''%(product[0])
+                cr.execute(sql)
+                categ_ids += [r[0] for r in cr.fetchall()]
+                return self.pool.get('product.product').browse(cr,uid,categ_ids)
+            if not product and not categ :
+                sql='''
+                            select product_product.id 
+                            from product_product,product_template 
+                            where product_template.categ_id in(select product_category.id from product_category) 
+                            and product_product.product_tmpl_id = product_template.id  ;
+                '''
+                cr.execute(sql)
+                categ_ids += [r[0] for r in cr.fetchall()]
+                return self.pool.get('product.product').browse(cr,uid,categ_ids)
+            
+        def get_ton_sl(line):
+            loc = o.location_id
+            location = self.pool.get('stock.location').browse(cr,uid,loc[0])
+            sql = '''
+                            select case when sum(foo.product_qty)>0 then sum(foo.product_qty) else 0 end ton_sl from 
+                                (select st.product_qty
+                                    from stock_move st 
+                                    where st.state='done' and st.product_id = %s and st.location_dest_id = %s
+                                union all
+                                select st.product_qty*-1
+                                    from stock_move st 
+                                    where st.state='done' and st.product_id = %s and st.location_id = %s
+                                )foo
+                        '''%(line.id,location.id,line.id,location.id)
+            cr.execute(sql)
+            ton_sl = cr.dictfetchone()
+            return ton_sl and ton_sl['ton_sl'] or 0
+        
+        def get_ins_qty(line):
+            loc = o.location_id
+            location = self.pool.get('stock.location').browse(cr,uid,loc[0])
+            parent_ids = self.pool.get('stock.location').search(cr, uid, [('name','=','Quality Inspection'),('usage','=','view')])
+            locat_ids = self.pool.get('stock.location').search(cr, uid, [('name','in',['Inspection']),('location_id','=',parent_ids[0])])
+            sql = '''
+                            select case when sum(foo.product_qty)>0 then sum(foo.product_qty) else 0 end ton from 
+                                (select st.product_qty
+                                    from stock_move st 
+                                    where st.state='done' and st.product_id = %s and st.location_dest_id = %s
+                                union all
+                                select st.product_qty*-1
+                                    from stock_move st 
+                                    where st.state='done' and st.product_id = %s and st.location_id = %s
+                                )foo
+                        '''%(line.id,locat_ids[0],line.id,locat_ids[0])
+            cr.execute(sql)
+            ton = cr.dictfetchone()
+            return ton and ton['ton'] or 0
+        def get_blo_qty(line):
+            loc = o.location_id
+            location = self.pool.get('stock.location').browse(cr,uid,loc[0])
+            parent_ids = self.pool.get('stock.location').search(cr, uid, [('name','in',['Block List','Block','Blocked List','Blocked']),('usage','=','view')])
+            locat_ids = self.pool.get('stock.location').search(cr, uid, [('name','in',['Block List','Block','Blocked List','Blocked']),('location_id','=',parent_ids[0])])
+            sql = '''
+                            select case when sum(foo.product_qty)>0 then sum(foo.product_qty) else 0 end ton from 
+                                (select st.product_qty
+                                    from stock_move st 
+                                    where st.state='done' and st.product_id = %s and st.location_dest_id = %s
+                                union all
+                                select st.product_qty*-1
+                                    from stock_move st 
+                                    where st.state='done' and st.product_id = %s and st.location_id = %s
+                                )foo
+                        '''%(line.id,locat_ids[0],line.id,locat_ids[0])
+            cr.execute(sql)
+            ton = cr.dictfetchone()
+            return ton and ton['ton'] or 0 
+        
+        cr.execute('delete from tpt_stock_on_hand')
+        stock_obj = self.pool.get('tpt.stock.on.hand')
+        stock = self.browse(cr, uid, ids[0])
+        stock_line = []
+        for line in get_categ(stock):
+            stock_line.append((0,0,{
+                'code': line.code or False,
+                'description': line.name or False,
+                'uom': line.uom_id and line.uom_id.name or False,
+                'bin_loc': line.bin_location or False,
+                'onhand_qty': get_ton_sl(line),
+                'ins_qty': get_ins_qty(line),
+                'bl_qty': get_blo_qty(line),
+                
+            }))
+        vals = {
+            'name': 'STOCK ON HAND REPORT',
+            'categ_id': stock.categ_id and stock.categ_id.id or False,
+            'product_id': stock.product_id and stock.product_id.id or False,
+            'location_id': stock.location_id and stock.location_id.id or False,
+            'stock_line': stock_line,
+            'as_date': time.strftime('%Y-%m-%d'),
+        }
+        stock_id = stock_obj.create(cr, uid, vals)
+        res = self.pool.get('ir.model.data').get_object_reference(cr, uid, 
+                                        'green_erp_arulmani_purchase', 'view_tpt_stock_on_hand_form')
+        return {
+                    'name': 'STOCK ON HAND REPORT',
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'res_model': 'tpt.stock.on.hand',
+                    'domain': [],
+                    'type': 'ir.actions.act_window',
+                    'target': 'current',
+                    'res_id': stock_id,
+                }
+    
 stock_on_hand_report()
 
 class product_product(osv.osv):
