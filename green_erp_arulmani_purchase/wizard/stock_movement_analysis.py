855872914b1242830df343dcc8014a4eff7c03a0
TrongNguyen <nguyentrong6193@gmail.com>
2015-05-30 18:07:25 +0700

    update

--- green_erp_arulmani_purchase/wizard/stock_movement_analysis.py
+++ green_erp_arulmani_purchase/wizard/stock_movement_analysis.py
@@ -55,7 +55,36 @@ class stock_movement_analysis(osv.osv_memory):
         datas['model'] = 'stock.movement.analysis'
         datas['form'] = self.read(cr, uid, ids)[0]
         datas['form'].update({'active_id':context.get('active_ids',False)})
-        return {'type': 'ir.actions.report.xml', 'report_name': 'report_stock_movement_analysis', 'datas': datas}
+        return {'type': 'ir.actions.report.xml', 'report_name': 'report_stock_movement_analysis_xls', 'datas': datas}
+#             stock_obj = self.pool.get('tpt.form.movement.analysis')
+#             cr.execute('delete from tpt_form_movement_analysis')
+#             stock = self.browse(cr, uid, ids[0])
+#             vals = {
+#                 'name': 'Stock Movement Analysis ',
+#                 'product_id': stock.product_id.id,
+#                 'product_name': stock.product_id and stock.product_id.name or 'All' ,
+#                 'product_name_title': 'Product',
+#                 'date_from': stock.date_from,
+#                 'date_to': stock.date_to,
+#                 'date_from_title':'Date From: ',
+#                 'date_to_title':'To: ',
+#                 'categ_id': stock.categ_id.id,
+#                 'categ_name': stock_obj.get_categ_name(cr, uid, ids,stock.categ_id.cate_name),
+#                 'categ_name_title': 'Product Category',
+#             }
+#             stock_id = stock_obj.create(cr, uid, vals)
+#             res = self.pool.get('ir.model.data').get_object_reference(cr, uid, 
+#                                             'green_erp_arulmani_purchase', 'view_tpt_form_movement_analysis')
+#             return {
+#                         'name': 'Stock Movement Analysis',
+#                         'view_type': 'form',
+#                         'view_mode': 'form',
+#                         'res_model': 'tpt.form.movement.analysis',
+#                         'domain': [],
+#                         'type': 'ir.actions.act_window',
+#                         'target': 'current',
+#                         'res_id': stock_id,
+#                     }
         
 stock_movement_analysis()
 
@@ -81,46 +110,74 @@ class product_product(osv.osv):
     def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=100):
        ids = self.search(cr, user, args, context=context, limit=limit)
        return self.name_get(cr, user, ids, context=context)
+product_product()
 
-class stock_location(osv.osv):
-    _inherit = "stock.location"
+class tpt_form_movement_analysis(osv.osv_memory):
+    _name = "tpt.form.movement.analysis"
+    _columns = {    
+                'categ_id': fields.many2one('product.category', 'Product Category'),
+                'product_id': fields.many2one('product.product', 'Product'),
+                'date_from':fields.date('Date From'),
+                'date_from_title':fields.char('Date From'),
+                'date_to':fields.date('To'),
+                'date_to_title':fields.char('To'),
+                'name':fields.char('Stock Movement Analysis',size=1024,readonly=True),
+                'movement_line':fields.one2many('movement_analysis_line','movement_id','Stock Movement'),
+                'categ_name':fields.char('Product Category',size=1024),
+                'product_name':fields.char('Product',size=1024),
+                'categ_name_title':fields.char('Product Category',size=1024),
+                'product_name_title':fields.char('Product Category',size=1024),
+                }
+
+
+
+    def get_categ_name(self, cr, uid, ids,categ_name = False, context=None):
+        if categ_name and categ_name == 'raw':
+            cate_name = 'Raw Materials'
+        if categ_name and categ_name == 'spares':
+            cate_name = 'Spares'
+        return cate_name
     
-    def search(self, cr, uid, args, offset=0, limit=None, order=None, context=None, count=False):
+    def print_xls(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
-        if context.get('search_location_id'):
-            if context.get('product_id'):
-                sql = '''
-                     select case when sum(foo.product_qty)>0 then sum(foo.product_qty) else 0 end ton_sl from 
-                            (select st.product_qty
-                                from stock_move st 
-                                where st.state='done' and st.product_id=%s and st.location_dest_id in (select id from stock_location
-                                                                                        where usage = 'internal')
-                            union all
-                            select st.product_qty*-1
-                                from stock_move st 
-                                where st.state='done' and st.product_id=%s and st.location_id in (select id from stock_location
-                                                                                        where usage = 'internal')
-                            )foo
-                '''%(context.get('product_id'), context.get('product_id'))
-                cr.execute(sql)
-                ton_sl = cr.dictfetchone()['ton_sl']
-                if ton_sl > 0:
-                    sql = '''
-                        select location_dest_id from stock_move where state = 'done' and product_id = %s and location_dest_id in (select id from stock_location
-                                                                                        where usage = 'internal')
-                    '''%(context.get('product_id'))
-                    cr.execute(sql)
-                    location_ids = [row[0] for row in cr.fetchall()]
-                    args += [('id','in',location_ids)]
-                else:
-                    location_ids = []
-                    raise osv.except_osv(_('Warning!'),_('Not exist any quantity in stock for this product!'))
-                    args += [('id','in',location_ids)]
-        return super(stock_location, self).search(cr, uid, args, offset=offset, limit=limit, order=order, context=context, count=count)
+        datas = {'ids': context.get('active_ids', [])}
+        datas['model'] = 'tpt.form.movement.analysis'
+        datas['form'] = self.read(cr, uid, ids)[0]
+        datas['form'].update({'active_id':context.get('active_ids',False)})
+        return {'type': 'ir.actions.report.xml', 'report_name': 'report_stock_movement_analysis_xls', 'datas': datas}
+    def print_pdf(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        datas = {'ids': context.get('active_ids', [])}
+        datas['model'] = 'tpt.form.movement.analysis'
+        datas['form'] = self.read(cr, uid, ids)[0]
+        datas['form'].update({'active_id':context.get('active_ids',False)})
+        return {'type': 'ir.actions.report.xml', 'report_name': 'report_stock_movement_analysis_pdf', 'datas': datas}
+tpt_form_movement_analysis()
 
-    def name_search(self, cr, user, name, args=None, operator='ilike', context=None, limit=100):
-       ids = self.search(cr, user, args, context=context, limit=limit)
-       return self.name_get(cr, user, ids, context=context)
 
-stock_location
+class tpt_movement_analysis_line(osv.osv):
+    _name = "tpt.movement.analysis.line"
+    _columns = {    
+        'movement_id': fields.many2one('tpt.form.movement.analysis', 'Stock Movement', ondelete='cascade'),
+        'item_code': fields.char('Item Code', size = 1024),
+        'item_name': fields.char('Item Name', size = 1024),
+        'uom': fields.char('UOM', size = 1024),
+        'open_stock': fields.float('Opening Stock'),
+        'open_value': fields.float('Opening Stock Value'),
+        'receipt_qty': fields.float('Qty (Receipts)'),
+        'receipt_value':fields.float('Stock Value (Receipts)'),
+        'consum_qty': fields.float('Qty (Consumption)'),
+        'consum_value':fields.float('Stock Value (Consumption)'),     
+        'close_stock': fields.float('Closing Stock'),
+        'close_value': fields.float('Closing Stock Value'),   
+                }
+    
+
+tpt_movement_analysis_line()
+
+
+
+
+
