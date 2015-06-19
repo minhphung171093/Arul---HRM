707ab79ffa2e4119cbe03bf7b34b01104ced3fec
TrongNguyen <nguyentrong6193@gmail.com>
2015-06-19 17:03:40 +0700

    update

--- green_erp_arulmani_purchase/wizard/stock_inward_outward_report.py
+++ green_erp_arulmani_purchase/wizard/stock_inward_outward_report.py
@@ -120,9 +120,9 @@ class stock_inward_outward_report(osv.osv_memory):
                                 join stock_location loc1 on st.location_id=loc1.id
                                 join stock_location loc2 on st.location_dest_id=loc2.id
                             where st.state='done' and st.location_dest_id=%s and st.product_id=%s
-                                and ( (picking_id in (select id from stock_picking where date < '%s' and state = 'done')) 
+                                and ( (picking_id in (select id from stock_picking where to_date(to_char(date, 'YYYY-MM-DD'), 'YYYY-MM-DD') < '%s' and state = 'done')) 
                                 or  (inspec_id in (select id from tpt_quanlity_inspection where date < '%s' and state in ('done','remaining')))
-                                or (st.id in (select move_id from stock_inventory_move_rel where inventory_id in (select id from stock_inventory where date <'%s' and state = 'done')))
+                                or (st.id in (select move_id from stock_inventory_move_rel where inventory_id in (select id from stock_inventory where to_date(to_char(date, 'YYYY-MM-DD'), 'YYYY-MM-DD') <'%s' and state = 'done')))
                                     )
                     '''%(locat_ids[0], product_id.id,date_from,date_from,date_from)
                 cr.execute(sql)
@@ -145,9 +145,9 @@ class stock_inward_outward_report(osv.osv_memory):
                                 join stock_location loc1 on st.location_id=loc1.id
                                 join stock_location loc2 on st.location_dest_id=loc2.id
                             where st.state='done' and st.location_dest_id=%s and st.product_id=%s
-                                and ( (picking_id in (select id from stock_picking where date < '%s' and state = 'done')) 
+                                and ( (picking_id in (select id from stock_picking where to_date(to_char(date, 'YYYY-MM-DD'), 'YYYY-MM-DD') < '%s' and state = 'done')) 
                                 or  (inspec_id in (select id from tpt_quanlity_inspection where date < '%s' and state in ('done','remaining')))
-                                or (st.id in (select move_id from stock_inventory_move_rel where inventory_id in (select id from stock_inventory where date <'%s' and state = 'done')))
+                                or (st.id in (select move_id from stock_inventory_move_rel where inventory_id in (select id from stock_inventory where to_date(to_char(date, 'YYYY-MM-DD'), 'YYYY-MM-DD') <'%s' and state = 'done')))
                                     )
                     '''%(locat_ids[0], product_id.id,date_from,date_from,date_from)
                 cr.execute(sql)
@@ -197,9 +197,9 @@ class stock_inward_outward_report(osv.osv_memory):
                             join stock_location loc1 on st.location_id=loc1.id
                             join stock_location loc2 on st.location_dest_id=loc2.id
                         where st.state='done' and st.location_dest_id=%s and st.product_id=%s
-                            and ( (picking_id in (select id from stock_picking where date < '%s' and state = 'done')) 
+                            and ( (picking_id in (select id from stock_picking where to_date(to_char(date, 'YYYY-MM-DD'), 'YYYY-MM-DD') < '%s' and state = 'done')) 
                             or  (inspec_id in (select id from tpt_quanlity_inspection where date < '%s' and state in ('done','remaining')))
-                            or (st.id in (select move_id from stock_inventory_move_rel where inventory_id in (select id from stock_inventory where date <'%s' and state = 'done')))
+                            or (st.id in (select move_id from stock_inventory_move_rel where inventory_id in (select id from stock_inventory where to_date(to_char(date, 'YYYY-MM-DD'), 'YYYY-MM-DD') <'%s' and state = 'done')))
                                 )
                '''%(locat_ids[0],product_id.id, date_from, date_from, date_from)
                cr.execute(sql)
@@ -232,9 +232,9 @@ class stock_inward_outward_report(osv.osv_memory):
                             join stock_location loc1 on st.location_id=loc1.id
                             join stock_location loc2 on st.location_dest_id=loc2.id
                         where st.state='done' and st.location_dest_id=%s and st.product_id=%s
-                            and ( (picking_id in (select id from stock_picking where date < '%s' and state = 'done')) 
+                            and ( (picking_id in (select id from stock_picking where to_date(to_char(date, 'YYYY-MM-DD'), 'YYYY-MM-DD') < '%s' and state = 'done')) 
                             or  (inspec_id in (select id from tpt_quanlity_inspection where date < '%s' and state in ('done','remaining')))
-                            or (st.id in (select move_id from stock_inventory_move_rel where inventory_id in (select id from stock_inventory where date <'%s' and state = 'done')))
+                            or (st.id in (select move_id from stock_inventory_move_rel where inventory_id in (select id from stock_inventory where to_date(to_char(date, 'YYYY-MM-DD'), 'YYYY-MM-DD') <'%s' and state = 'done')))
                                 )
                '''%(locat_ids[0],product_id.id, date_from, date_from,date_from)
                cr.execute(sql)
