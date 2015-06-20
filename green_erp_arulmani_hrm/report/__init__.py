1a455301d99517db14e971189a8f5f3ff3167a09
BalamuruganPurushothaman <balamurugan.purushothaman@tenthplanettechnologies.com>
2015-06-20 20:44:46 +0530

    TPT-HR Employee Leave Balance Report

--- green_erp_arulmani_hrm/report/__init__.py
+++ green_erp_arulmani_hrm/report/__init__.py
@@ -6,3 +6,4 @@ import gate_pass
 import tpt_time_evaluation_report
 import epf_statement_report
 import esi_statement_report
+import leave_balance_report
--- green_erp_arulmani_hrm/wizard/__init__.py
+++ green_erp_arulmani_hrm/wizard/__init__.py
@@ -5,4 +5,5 @@ import alert_form
 import time_evaluation_report
 import epf_statement_form
 import esi_statement
+import leave_balance_form
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
