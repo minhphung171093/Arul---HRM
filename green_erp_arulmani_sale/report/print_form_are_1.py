2f4173acd3ee6b4833ef78ef9d2e104fcefbcb86
BalamuruganPurushothaman <balamurugan.purushothaman@tenthplanettechnologies.com>
2015-07-16 22:47:40 +0530

    TPT-Sales RE print1

--- green_erp_arulmani_sale/report/print_form_are_1.py
+++ green_erp_arulmani_sale/report/print_form_are_1.py
@@ -31,7 +31,12 @@ class Parser(report_sxw.rml_parse):
             'get_arename':self.get_arename,
             'get_amt':self.get_amt,
             'get_edamt':self.get_edamt,
+            'get_dec':self.get_dec,
         })
+    def get_dec(self,number=False):
+        a = round(number - int(number), 2)
+        a = format(a, '.2f') 
+        return a[2:]
     def get_amt(self,value=False):
         locale.setlocale(locale.LC_NUMERIC, "en_IN")
         inr_comma_format = locale.format("%.0f", value, grouping=True)
