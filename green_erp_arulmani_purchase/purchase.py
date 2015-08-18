ecfdec0547737e2085bf804d3e8fb9059a958b75
BalamuruganPurushothaman <balamurugan.purushothaman@tenthplanettechnologies.com>
2015-08-18 17:15:50 +0530

    TPT-Productstore on hand qty

--- green_erp_arulmani_purchase/purchase.py
+++ green_erp_arulmani_purchase/purchase.py
@@ -863,6 +863,73 @@ class product_product(osv.osv):
                     time_total=0.0
             res[time.id]['onhand_qty'] = time_total            
         return res
+    def _onhand_qty_store(self, cr, uid, ids, field_name, arg, context=None):
+        res = {}
+        for prod in self.browse(cr, uid, ids, context=context):
+            res[prod.id] = {
+                'onhand_qty_store': 0.0,
+            }
+            if prod.id : 
+                flag = False
+                if prod.categ_id.name=='RawMaterials':
+                    categ='Raw Material'
+                    flag = True
+                if prod.categ_id.name =='FinishedProduct':   
+                    categ='Finished Product'
+                    flag = True
+                    
+                if flag is True:
+                    prod_categ =  categ
+                else:
+                    prod_categ =   prod.categ_id.name
+                    
+                sql = '''
+                select sum(foo.product_qty) as ton_sl from 
+                    (select l2.id as loc,st.prodlot_id,pu.id,st.product_qty
+                        from stock_move st 
+                            inner join stock_location l2 on st.location_dest_id= l2.id
+                            inner join product_uom pu on st.product_uom = pu.id
+                        where st.state='done' and st.product_id=%s and l2.usage = 'internal'
+                    union all
+                    select l1.id as loc,st.prodlot_id,pu.id,st.product_qty*-1
+                        from stock_move st 
+                            inner join stock_location l1 on st.location_id= l1.id
+                            inner join product_uom pu on st.product_uom = pu.id
+                        where st.state='done' and st.product_id=%s and l1.usage = 'internal'
+                    )foo
+                    where foo.loc in (select id from stock_location where name='%s'
+                    and location_id in (select id from stock_location where name='Store'))
+                    group by foo.loc,foo.prodlot_id,foo.id
+                '''%(prod.id,prod.id, prod_categ)
+                #===============================================================
+                # sql1 = '''
+                # SELECT sum(onhand_qty) onhand_qty
+                # From
+                # (SELECT
+                #        
+                #     case when loc1.usage != 'internal' and loc2.usage = 'internal'
+                #     then stm.primary_qty
+                #     else
+                #     case when loc1.usage = 'internal' and loc2.usage != 'internal'
+                #     then -1*stm.primary_qty 
+                #     else 0.0 end
+                #     end onhand_qty
+                #             
+                # FROM stock_move stm 
+                #     join stock_location loc1 on stm.location_id=loc1.id
+                #     join stock_location loc2 on stm.location_dest_id=loc2.id
+                # WHERE stm.state= 'done' and product_id=%s)foo
+                #    '''% (time.id)
+                #===============================================================
+                cr.execute(sql)
+                a = cr.fetchone()
+                if a:
+                    time_total = a[0]                            
+                else:
+                    time_total=0.0
+            res[prod.id]['onhand_qty_store'] = time_total            
+        return res
+    
     _columns = {
         'description':fields.text('Description'),
         'batch_appli_ok':fields.boolean('Is Batch Applicable'),
@@ -895,7 +962,8 @@ class product_product(osv.osv):
                                            ('raw_mat','Raw. Mat. & Prod'),
                                            ('qc','QC and R&D'),
                                            ('safe','Safety & Personnel'),('proj','Projects')],'Material Type'),
-        'onhand_qty': fields.function(_onhand_qty, string='OnHand Qty', multi='test_qty'),
+        'onhand_qty': fields.function(_onhand_qty, string='On-Hand Qty', multi='test_qty'),
+        'onhand_qty_store': fields.function(_onhand_qty_store, string='Store On-Hand Qty', multi='test_qty1'),
         
         'tolerance_qty': fields.float('Tolerance'), #TPT
         
