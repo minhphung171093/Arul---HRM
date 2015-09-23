bb1b51ad44e23f8c152c5cda7e6af5a820f8c8a8
tien <tien@tien-123.(none)>
2015-09-23 17:01:04 +0700

    up

--- green_erp_arulmani_update/update.py
+++ green_erp_arulmani_update/update.py
@@ -3709,7 +3709,7 @@ class tpt_update_stock_move_report(osv.osv):
                     price_ids = cr.fetchone()
                     produce_price = (price_ids and price_ids[0] or 0) * (produce.product_qty or 0)
                     debit -= produce_price
-            for act in line.bom_id.activities_line:
+            for act in line.activities_line:
                 debit += act.product_cost or 0
             if debit:
                 unit_produce = debit/line.product_qty
@@ -3934,7 +3934,7 @@ class tpt_update_stock_move_report(osv.osv):
                                                }))
                     else:
                         raise osv.except_osv(_('Warning!'),_("Product Asset Account is not configured for Product '%s'! Please configured it!")%(produce.product_id.default_code))
-            for act in line.bom_id.activities_line:
+            for act in line.activities_line:
                 if line.product_id.product_credit_id:
 #                         credit += act.product_cost
                     debit += act.product_cost and round(act.product_cost,2) or 0
