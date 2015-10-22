fdb44bc361f828c63dbefc2eccb7a1383bf89dce
BalamuruganPurushothaman <balamurugan.purushothaman@tenthplanettechnologies.com>
2015-10-22 18:23:04 +0530

    TPT-Maintenance Module

--- green_erp_arulmani_maintenance/__init__.py
+++ green_erp_arulmani_maintenance/__init__.py
@@ -4,4 +4,5 @@
 #
 ##############################################################################
 import tpt_maintenance
+import report
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
