e601181ea64bf0c9f7e968d53a723240ec76f03c
BalamuruganPurushothaman <balamurugan.purushothaman@tenthplanettechnologies.com>
2015-12-29 11:23:36 +0530

    TPT-TDS Report

--- green_erp_arulmani_accounting/wizard/tds_form.py
+++ green_erp_arulmani_accounting/wizard/tds_form.py
@@ -203,12 +203,15 @@ class tds_form_report(osv.osv_memory):
                         ai.name as invoicedocno, ai.date_invoice as postingdate,
                         ai.bill_number as bill_no,ai.bill_date as bill_date,
                         ail.amount_basic as base_amnt, at.name as tax_deduction, 
-                        cast(round((ail.amount_basic)*at.amount/100,0) As decimal(8, 2)) as tdsamount, 
-                        --case when am.doc_type ='sup_inv_po' then cast(round((ail.amount_basic)*at.amount/100,0) As decimal(8, 2))
-                        --when am.doc_type ='ser_inv' then ai.amount_total_tds
-                        --when am.doc_type ='sup_inv' then ai.amount_total_tds
-                        --when am.doc_type ='freight' then ai.amount_total_tds_2
-                        --else 0 end as tdsamount,
+                        --cast(round((ail.amount_basic)*at.amount/100,0) As decimal(8, 2)) as tdsamount, 
+                        case when  am.doc_type='freight' then
+                        case when ail.fright_fi_type='3' then
+                        cast(round((cast(coalesce(ail.fright,0) As decimal(8,2))*quantity)*at.amount/100,0) As decimal(8, 2))
+                        else
+                        cast(round((cast(coalesce(ail.fright,0) As decimal(8,2)))*at.amount/100,0) As decimal(8, 2))
+                        end
+                        else cast(round((ail.amount_basic)*at.amount/100,0) As decimal(8, 2))
+                        end as tdsamount,
                         ai.vendor_ref as ven_ref, ai.number as gl_doc,at.section as sec
                         from account_invoice_line ail
                         join account_invoice ai on (ai.id=ail.invoice_id)
