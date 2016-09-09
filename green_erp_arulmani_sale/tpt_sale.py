598f6ea18073a38442a9fbd2081afbb229fa8cfd
bm <dev.bala05@gmail.com>
2016-09-09 18:06:32 +0530

    tpt-add

--- green_erp_arulmani_sale/tpt_sale.py
+++ green_erp_arulmani_sale/tpt_sale.py
@@ -2866,24 +2866,22 @@ class tpt_cus_consignee(osv.osv):
                     }
         return {'value': vals} 
     
-    #===========================================================================
-    # def name_get(self, cr, uid, ids, context=None):
-    #     res = []
-    #     if isinstance(ids, (int, long)):
-    #         ids = [ids]
-    #     if not ids:
-    #         return res
-    #     reads = self.read(cr, uid, ids, ['tpt_consignee_id','tpt_consignee_code'], context)
-    #     for record in reads:
-    #         name = ''
-    #         if record['tpt_consignee_code']:
-    #             name += record['tpt_consignee_code'][0:6]+'_'
-    #         if record['tpt_consignee_id']:
-    #             name += record['tpt_consignee_id'][1]
-    #         
-    #         res.append((record['id'], name))
-    #     return res
-    #===========================================================================
+    def name_get(self, cr, uid, ids, context=None):
+        res = []
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        if not ids:
+            return res
+        reads = self.read(cr, uid, ids, ['tpt_consignee_id','tpt_consignee_code'], context)
+        for record in reads:
+            name = ''
+            if record['tpt_consignee_code']:
+                name += record['tpt_consignee_code'][0:6]+'_'
+            if record['tpt_consignee_id']:
+                name += record['tpt_consignee_id'][1]
+             
+            res.append((record['id'], name))
+        return res
     ###
     def create(self, cr, uid, vals, context=None):
         if 'tpt_consignee_id' in vals:
