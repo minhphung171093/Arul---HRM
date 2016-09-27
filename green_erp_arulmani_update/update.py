be2827d7a836c241da40297e868a46be45d2673b
bm <dev.bala05@gmail.com>
2016-09-27 10:01:09 +0530

    tpt

--- green_erp_arulmani_update/update.py
+++ green_erp_arulmani_update/update.py
@@ -4649,6 +4649,54 @@ class tpt_update_stock_move_report(osv.osv):
                         
      
         return self.write(cr, uid, ids, {'result':'TPT update ISSUE for report Done'})
+    def adj_goods_issue_raw_sup_alu_sul(self, cr, uid, ids, context=None):
+        temp_obj = self.pool.get('tpt.aml.sl.line')
+        sql = '''
+            select sm.id, pp.default_code, sm.product_qty, sm.price_unit, sm.issue_id, round(sm.product_qty*sm.price_unit, 2) as total  
+            from stock_move sm
+            inner join product_product pp on sm.product_id=pp.id
+            where to_date(to_char(sm.date, 'YYYY-MM-DD'), 'YYYY-MM-DD') between '2015-04-01' and '2016-03-31'
+            and pp.cate_name='raw' and sm.issue_id is not null and sm.state='done' and sm.price_unit>=0
+            and sm.product_id=10736
+            order by sm.issue_id
+        '''
+        cr.execute(sql)
+        vals = {}
+            
+        for ma in cr.dictfetchall():
+            sql = '''
+            select aml.id, aml.account_id from account_move_line aml
+            inner join account_move am on aml.move_id=am.id
+            where am.material_issue_id=%s --and aml.debit>0
+            and aml.id not in (select aml_id from tpt_aml_sl_line)
+            and aml.account_id in (3470, 4502)
+            order by aml.id limit 2
+            '''%ma['issue_id']
+            cr.execute(sql)
+
+            for aml in cr.dictfetchall():
+                temp_ids = temp_obj.search(cr, uid, [('aml_id','=',aml['id'])])
+                if not temp_ids:
+                    print ma['issue_id']
+                    if aml['account_id']==3470:# 0000119403 - RM-SULPHURIC ACID 
+                        sql = '''
+                        update account_move_line set credit=%s where id=%s 
+                        '''%(ma['total'], aml['id'])
+                        cr.execute(sql)
+                        if cr.rowcount>0:
+                            vals['aml_id'] = aml['id']
+                            temp_obj.create(cr, uid, vals, context)
+                    if aml['account_id']==4502:#0009900028 - SULPHURIC ACID - Cons. 
+                        sql = '''
+                        update account_move_line set debit=%s where id=%s 
+                        '''%(ma['total'], aml['id'])
+                        cr.execute(sql)
+                        if cr.rowcount>0:
+                            vals['aml_id'] = aml['id']
+                            temp_obj.create(cr, uid, vals, context)
+                        
+     
+        return self.write(cr, uid, ids, {'result':'TPT update ISSUE for report Done'})
     def adj_third_permission(self, cr, uid, ids, context=None):
         temp_obj = self.pool.get('tpt.aml.sl.line')
         sql = '''
@@ -4862,6 +4910,8 @@ class tpt_update_avg_cost(osv.osv):
                     cr.execute(sql)
                     open_hand_qty = cr.fetchone()[0]
                     #
+                    cons_qty = 0
+                    cons_value = 0
                     sql = '''
                     select sm.product_qty, sm.price_unit, mi.doc_no from stock_move sm
                     inner join tpt_material_issue mi on sm.issue_id=mi.id
